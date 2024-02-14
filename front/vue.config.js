0b38c85863ced58cf94d076f7bc7ddec3e515599
mobilepig <mobilepig@naver.com>
2024-02-14 20:32:36 +0900

    2024/02/14 / separate api dev and production / sheep changsuk

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  devServer: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        pathRewrite: {
-          "^/api": "",
-        },
-      },
-    },
-  },
   configureWebpack: (config) => {
     config.devServer = config.devServer || {};
     if (process.env.NODE_ENV !== "development") {
+      config.devServer.proxy = {
+        "/api": {
+          //여기에 배포 클러스터
+          target: "http://localhost:3000",
+          changeOrigin: true,
+          pathRewrite: {
+            "^/api": "",
+          },
+        },
+      };
+    } else {
       config.devServer.proxy = {
         "/api": {
           //여기에 배포 클러스터
