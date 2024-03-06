0f9ff1dadf686002c44586937cd01baf97ce9b36
mobilepig <mobilepig@naver.com>
2024-03-06 14:52:43 +0900

    test

--- front/src/App.vue
+++ front/src/App.vue
@@ -255,7 +255,7 @@ export default {
       logOutUrl:
         process.env.NODE_ENV == "development"
           ? "https://test-dom.auth.ap-northeast-2.amazoncognito.com/logout?client_id=6crfr295su16kvf3uta0t29vi9&logout_uri=http://localhost:8080/logout"
-          : "https://dga-dom.auth.ap-northeast-2.amazoncognito.com/logout?client_id=3jpsb7a7osuntcg2rdr7des17q&logout_uri=https://www.daddygo.vacations/logout",
+          : "https://bam-dom.auth.ap-northeast-2.amazoncognito.com/logout?client_id=3jpsb7a7osuntcg2rdr7des17q&logout_uri=https://www.daddygo.vacations/logout",
     });
 
     const removeUserInfo = () => {
