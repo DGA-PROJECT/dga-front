4ccfe8d08dfcdce0973d4b5bb84b4bd4c8e6dbd2
mobilepig <mobilepig@naver.com>
2024-03-06 11:06:03 +0900

    cash

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,58 +1,45 @@
-module.exports = {
+const { defineConfig } = require("@vue/cli-service");
+
+module.exports = defineConfig({
+  filenameHashing: true,
   configureWebpack: (config) => {
+    config.output.filename = "js/[name].[hash].js";
+    config.output.chunkFilename = "js/[name].[hash].js";
+
+    // 추가한 부분 시작
     config.devServer = config.devServer || {};
-    if (process.env.NODE_ENV == "development") {
-      config.devServer.proxy = {
-        "/api/users": {
-          //여기에 배포 클러스터
-          target: "http://localhost:3000",
-          changeOrigin: true,
-          pathRewrite: {
-            "^/api/users": "",
-          },
+    config.devServer.proxy = {
+      "/api/users": {
+        target:
+          process.env.NODE_ENV === "development"
+            ? "http://localhost:3000"
+            : "https://www.daddygo.vacations/api/users",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api/users": "",
         },
-        "/api/leaderboards": {
-          //여기에 배포 클러스터
-          target: "http://localhost:3001",
-          changeOrigin: true,
-          pathRewrite: {
-            "^/api/leaderboards": "",
-          },
+      },
+      "/api/leaderboards": {
+        target:
+          process.env.NODE_ENV === "development"
+            ? "http://localhost:3001"
+            : "https://www.daddygo.vacations/api/leaderboards",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api/leaderboards": "",
         },
-        "/api/search": {
-          //여기에 배포 클러스터
-          target: "http://localhost:3002",
-          changeOrigin: true,
-          pathRewrite: {
-            "^/api/searches": "",
-          },
+      },
+      "/api/search": {
+        target:
+          process.env.NODE_ENV === "development"
+            ? "http://localhost:3002"
+            : "https://www.daddygo.vacations/api/search",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api/searches": "",
         },
-      };
-    } else {
-      config.devServer.proxy = {
-        "/api/users": {
-          //여기에 배포 클러스터
-          target: "https://www.daddygo.vacations/api/users",
-          changeOrigin: true,
-          pathRewrite: {
-            "^/api": "",
-          },
-        },
-        "/api/leaderboards": {
-          target: "https://www.daddygo.vacations/api/leaderboards",
-          changeOrigin: true,
-          pathRewrite: {
-            "^/api": "",
-          },
-        },
-        "/api/search": {
-          target: "https://www.daddygo.vacations/api/search",
-          changeOrigin: true,
-          pathRewrite: {
-            "^/api": "",
-          },
-        },
-      };
-    }
+      },
+    };
+    // 추가한 부분 끝
   },
-};
+});
